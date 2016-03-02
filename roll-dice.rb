d275a62cfbdb7a34ccb10b296cbaa34a6637edb2
Dominick Triola <dominick@ignovadesign.com>
2016-03-01 19:34:27 -0800

    added option to choose number of sides on dice in roll-dice.rb

--- roll-dice.rb
+++ roll-dice.rb
@@ -1,15 +1,18 @@
 # A function to roll any number of 6 sided dice
-def roll_dice(num)
+def roll_dice(rolls, sides)
 
 	result = 0
+	die = []
 
-	die = [1,2,3,4,5,6]
+	i=1
+	while i < (sides + 1)
+		die << i
+		i+=1
+	end
 
 	i=0
-	while i < num
-
+	while i < rolls
 		result += die.sample
-
 		i+=1
 	end
 
@@ -18,5 +21,7 @@ def roll_dice(num)
 
 end
 
-roll_dice(1) # => rolls 1 die
-roll_dice(2) # => rolls 2 dice
+roll_dice(1,6) # => rolls 1 six-sided die
+roll_dice(2,6) # => rolls 2 six-sided dice
+roll_dice(1,2) # => rolls 1 two-sided die (like flipping a coin)
+roll_dice(1,20) # => rolls 1 twenty-sided die
