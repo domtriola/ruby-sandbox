f883cfb55b3ea9219dcd5ab188c231f57fd658d4
Dominick Triola <dominick@ignovadesign.com>
2016-03-30 23:05:48 -0700

    refactored

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,40 +1,27 @@
 #Alphabetically shifts the letters in a string by a specified amount
 def caesar(string, num)
 
-	arr = string.downcase.split(" ")
 	result = []
+	words = string.downcase.split(" ")
 
-	i = 0
-	while i < arr.length
-
-		word_num = []
-		arr[i].each_byte {|c| word_num << c}
-
+	words.each do |word|
 		new_chars = ""
-		word_idx = 0
-		while word_idx < word_num.length
-
-			if (word_num[word_idx] + num <= "z".ord)
-				new_chars += (word_num[word_idx] + num).chr
+		word.each_char do |char|
+			if (char.ord + num <= "z".ord)
+				new_chars += (char.ord + num).chr
 			else 
-				new_chars += ((word_num[word_idx] + num) - ("z".ord) + ("a".ord - 1)).chr
+				new_chars += ((char.ord + num) - "z".ord + ("a".ord - 1)).chr
 			end
-
-			word_idx += 1
 		end
 
 		result << new_chars
-
-		i+=1
 	end
 
-	puts result.join(" ")
-	return result.join(" ")
-
+	result.join(" ")
 end
 
 
-caesar("hello", 3) # => "khoor"
-caesar("hello world", 3)
-caesar("Zeus exits the arena", 3)
-caesar("Athena enters the arena", 12)
+p caesar("hello", 3) # => "khoor"
+p caesar("hello world", 3)
+p caesar("Zeus exits the arena", 3)
+p caesar("Athena enters the arena", 12)
