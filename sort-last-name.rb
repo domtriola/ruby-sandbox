2c08951a27fb468ddb429aefed9a0a9b25701d2e
Dominick Triola <dominick@ignovadesign.com>
2016-06-23 11:31:30 -0700

    add sort-last-name function

---
+++ sort-last-name.rb
@@ -0,0 +1,20 @@
+# Sort an array of names by last name using bubble sort
+def sort_by_last_name(array)
+  result = []
+  array.each do |name|
+    result << name.split(" ")
+  end
+  no_swap = false
+  until no_swap
+    result.each_with_index do |name, index|
+      no_swap = true
+      if result[index+1] && name[1] > result[index+1][1]
+        result[index], result[index+1] = result[index+1], result[index]
+        no_swap = false
+      end
+    end
+  end
+  result
+end
+
+p sort_by_last_name(["Arthur Dent", "Ford Prefect", "Zaphod Beeblebrox", "Sperm Whale", "Deep Thought"])
