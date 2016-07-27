2a41e8c00fab90a46bf01c0a29bdaa468304aa6f
Dominick Triola <dominick@ignovadesign.com>
2016-07-26 18:05:55 -0700

    refactor

--- igpay-atinlay.rb
+++ igpay-atinlay.rb
@@ -1,31 +1,32 @@
 # Convert a sentence into Pig Latin
-def igpay_atinlay(string)
-  vowels = %w{ a e i o u }
-  result = []
-  string.downcase.split(" ").each do |word|
-    if vowels.include?(word[0])
-      result << word + "ay"
-    else
-      chars_to_move = ""
-      vowel_found = false
-      until vowel_found || word == ""
-        if !vowels.include?(word[0])
-          chars_to_move += word[0]
-          word[0] = ""
-        elsif word[0] == "u" && chars_to_move[-1] == "q"
-          chars_to_move += word[0]
-          word[0] = ""
-        else
-          vowel_found = true
-        end
-      end
-      result << word + chars_to_move + "ay"
-    end
-  end
-  result.join(" ")
+def to_pig_latin(word)
+	word = word.downcase
+	vowels = %w{ a e i o u }
+	if vowels.include?(word[0])
+		return word + "ay"
+	else
+		chars_to_move = ""
+		vowel_found = false
+		until vowel_found || word == ""
+			if !vowels.include?(word[0])
+				chars_to_move += word[0]
+				word[0] = ""
+			elsif word[0] == "u" && chars_to_move[-1] == "q"
+				chars_to_move += word[0]
+				word[0] = ""
+			else
+				vowel_found = true
+			end
+		end
+		return word + chars_to_move + "ay"
+	end
 end
 
-p igpay_atinlay("This sentence has been converted into Pig Latin")
-p igpay_atinlay("I am sleepy")
-p igpay_atinlay("Goodnight")
-p igpay_atinlay("tst")
+def translate(string)
+  string.split(" ").map { |word| to_pig_latin(word) }.join(" ")
+end
+
+p translate("This sentence has been converted into Pig Latin")
+p translate("I am sleepy")
+p translate("Goodnight")
+p translate("tst")
