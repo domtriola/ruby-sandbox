6d8dde706778590a0dfe96709426b97f664294b2
Dominick Triola <dominick@ignovadesign.com>
2016-07-26 17:50:08 -0700

    reformat to a more robust solution

--- igpay-atinlay.rb
+++ igpay-atinlay.rb
@@ -1,37 +1,30 @@
 # Convert a sentence into Pig Latin
-def is_a_vowel?(char)
-	char = char.downcase
-	if (char == "a") || (char == "e") || (char == "i") || (char == "o") || (char == "u")
-		return true
-	else
-		return false
-	end
-end
-
-def igpay_atinlay(sentence)
-	
-	arr = sentence.downcase.split(" ")
-	result = []
-
-	arr.each do |item|
-
-		if is_a_vowel?(item[0])
-			atinlay_word = item + "way"
-		elsif is_a_vowel?(item[1])
-			first_letter = item.slice!(0)
-			atinlay_word = item + first_letter + "ay"
-		else
-			first_chars = item.slice!(0..1)
-			atinlay_word = item + first_chars + "ay"
-		end
-			
-		result << atinlay_word
-	end
-
-	result = result.join(" ")
+def igpay_atinlay(string)
+  vowels = %w{ a e i o u }
+  result = []
+  string.downcase.split(" ").each do |word|
+    if vowels.include?(word[0])
+      result << word + "ay"
+    else
+      chars_to_move = ""
+      vowel_found = false
+      until vowel_found
+        if !vowels.include?(word[0])
+          chars_to_move += word[0]
+          word[0] = ""
+        elsif word[0] == "u" && chars_to_move[-1] == "q"
+          chars_to_move += word[0]
+          word[0] = ""
+        else
+          vowel_found = true
+        end
+      end
+      result << word + chars_to_move + "ay"
+    end
+  end
+  result.join(" ")
 end
 
 p igpay_atinlay("This sentence has been converted into Pig Latin")
 p igpay_atinlay("I am sleepy")
 p igpay_atinlay("Goodnight")
-
