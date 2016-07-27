91cc3a3ed1363a8e57a438c7ff8860eed315b84b
Dominick Triola <dominick@ignovadesign.com>
2016-07-26 22:18:05 -0700

    let function handle punctuation and capitalization

--- igpay-atinlay.rb
+++ igpay-atinlay.rb
@@ -1,24 +1,39 @@
 # Convert a sentence into Pig Latin
 def to_pig_latin(word)
-	word = word.downcase
-	vowels = %w{ a e i o u }
-	if vowels.include?(word[0])
-		return word + "ay"
+  if word == word.capitalize
+    was_capitalized = true
+  else
+    was_capitalized = false
+  end
+  letters = word.downcase[/\w+/]
+  punctuation = word.gsub(/\w+/, "")
+  vowels = %w{ a e i o u }
+
+	if vowels.include?(letters[0])
+    if was_capitalized
+      return letters.capitalize + "ay" + punctuation
+    else
+      return letters + "ay"
+    end
 	else
 		chars_to_move = ""
 		vowel_found = false
-		until vowel_found || word == ""
-			if !vowels.include?(word[0])
-				chars_to_move += word[0]
-				word[0] = ""
-			elsif word[0] == "u" && chars_to_move[-1] == "q"
-				chars_to_move += word[0]
-				word[0] = ""
+		until vowel_found || letters == ""
+			if !vowels.include?(letters[0])
+				chars_to_move += letters[0]
+				letters[0] = ""
+			elsif letters[0] == "u" && chars_to_move[-1] == "q"
+				chars_to_move += letters[0]
+				letters[0] = ""
 			else
 				vowel_found = true
 			end
 		end
-		return word + chars_to_move + "ay"
+    if was_capitalized
+      return letters.capitalize + chars_to_move + "ay" + punctuation
+    else
+      return letters + chars_to_move + "ay" + punctuation
+    end
 	end
 end
 
@@ -30,3 +45,4 @@ p translate("This sentence has been converted into Pig Latin")
 p translate("I am sleepy")
 p translate("Goodnight")
 p translate("tst")
+p translate("testing... for punctuation, now?")
