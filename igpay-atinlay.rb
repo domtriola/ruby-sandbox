4bfb9cdd1531a15aac8c89ef222fbce779dbe653
Dominick Triola <dominick@ignovadesign.com>
2016-10-19 10:27:00 -0700

    refactor

--- igpay-atinlay.rb
+++ igpay-atinlay.rb
@@ -1,40 +1,12 @@
 # Convert a sentence into Pig Latin
 def to_pig_latin(word)
-  if word == word.capitalize
-    was_capitalized = true
-  else
-    was_capitalized = false
-  end
+  was_capitalized = word == word.capitalize ? true : false
   letters = word.downcase[/\w+/]
   punctuation = word.gsub(/\w+/, "")
-  vowels = %w{ a e i o u }
 
-	if vowels.include?(letters[0])
-    if was_capitalized
-      return letters.capitalize + "ay" + punctuation
-    else
-      return letters + "ay"
-    end
-	else
-		chars_to_move = ""
-		vowel_found = false
-		until vowel_found || letters == ""
-			if !vowels.include?(letters[0])
-				chars_to_move += letters[0]
-				letters[0] = ""
-			elsif letters[0] == "u" && chars_to_move[-1] == "q"
-				chars_to_move += letters[0]
-				letters[0] = ""
-			else
-				vowel_found = true
-			end
-		end
-    if was_capitalized
-      return letters.capitalize + chars_to_move + "ay" + punctuation
-    else
-      return letters + chars_to_move + "ay" + punctuation
-    end
-	end
+  first = letters.slice!(/\b\w*qu|\b[^aeiou]+/)
+  letters.capitalize! if was_capitalized
+  first ? letters + first + "ay" + punctuation : letters + "way" + punctuation
 end
 
 def translate(string)
